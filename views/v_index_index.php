c5971a1e02378d9e5d58191149d87a8387b4d441
J. Rhee <jrhee@fas.harvard.edu>
2013-11-05 10:11:31 -0800

    Second commit

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,3 +1,19 @@
+
+<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+
+<?php if($user): ?>
+
+    <pre>
+    <?php
+    print_r($user);
+    ?>
+    </pre>
+
+    Hello <?=$user->first_name;?>
+<?php else: ?>
+    Welcome to my app.  Please sign up or log in
+<?php endif; ?>
+
 <p>
 	Hello World! You have successfully spawned a new application.
 </p>
